228adb73e14b050e17f8f34b0c4da789b86cddf0
jaswanthmeesala <jaswanthjassu111@gmail.com>
2025-01-22 11:11:24 +0530

    updated players

--- Players/src/main/java/com/capstone/Players/model/User.java
+++ Players/src/main/java/com/capstone/Players/model/User.java
@@ -1,18 +1,23 @@
 package com.capstone.Players.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
-
+    private static int userCount=0;
     @Id
     private String userId;
+    @Indexed(unique = true)
     private String userName;
     private String userEmail;
     private String userPhone;
+    private String password;
+    private String role;
 
     private String userTeamId=null;
+    private int currentScore= 0;
     private int totalScore = 0;
     private int totalWickets = 0;
     private int totalMatches = 0;
@@ -21,76 +26,73 @@ public class User {
     private int numberOfMatchesOrganized = 0;
     private int numberOfSupportStaff = 0;
     private int numberOfSponsors = 0;
-    private String Role=null;
 
     // Default constructor
     public User() {}
 
     // Parameterized constructor
-    public User(String userId, String userName, String userEmail, String userPhone,
-                String userTeamId, int totalScore, int totalWickets, int totalMatches,
-                int highestScore, int highestWickets, int numberOfMatchesOrganized,
-                int numberOfSupportStaff, int numberOfSponsors, String Role) {
-        this.userId = userId;
+    public User(String userName, String userEmail, String userPhone, String role) {
+        userId="U"+(++userCount);
         this.userName = userName;
         this.userEmail = userEmail;
         this.userPhone = userPhone;
-        this.userTeamId = userTeamId;
-        this.totalScore = totalScore;
-        this.totalWickets = totalWickets;
-        this.totalMatches = totalMatches;
-        this.highestScore = highestScore;
-        this.highestWickets = highestWickets;
-        this.numberOfMatchesOrganized = numberOfMatchesOrganized;
-        this.numberOfSupportStaff = numberOfSupportStaff;
-        this.numberOfSponsors = numberOfSponsors;
-        this.Role = Role;
+        this.role = role;
     }
 
     // Getters and Setters
     // ... (same as previously defined)
+    
 
 
     public String getUserId() {
         return userId;
     }
 
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     public void setUserId(String userId) {
         this.userId = userId;
     }
-
+    
     public String getUserName() {
         return userName;
     }
-
+    
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
+    
     public String getUserEmail() {
         return userEmail;
     }
-
+    
     public void setUserEmail(String userEmail) {
         this.userEmail = userEmail;
     }
-
+    
     public String getUserPhone() {
         return userPhone;
     }
-
+    
     public void setUserPhone(String userPhone) {
         this.userPhone = userPhone;
     }
-
+    
     public String getUserTeamId() {
         return userTeamId;
     }
-
+    
     public void setUserTeamId(String userTeamId) {
         this.userTeamId = userTeamId;
     }
-
+    
     public int getTotalScore() {
         return totalScore;
     }
@@ -98,31 +100,31 @@ public class User {
     public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
     }
-
+    
     public int getTotalWickets() {
         return totalWickets;
     }
-
+    
     public void setTotalWickets(int totalWickets) {
         this.totalWickets = totalWickets;
     }
-
+    
     public int getTotalMatches() {
         return totalMatches;
     }
-
+    
     public void setTotalMatches(int totalMatches) {
         this.totalMatches = totalMatches;
     }
-
+    
     public int getHighestScore() {
         return highestScore;
     }
-
+    
     public void setHighestScore(int highestScore) {
         this.highestScore = highestScore;
     }
-
+    
     public int getHighestWickets() {
         return highestWickets;
     }
@@ -134,15 +136,15 @@ public class User {
     public int getNumberOfMatchesOrganized() {
         return numberOfMatchesOrganized;
     }
-
+    
     public void setNumberOfMatchesOrganized(int numberOfMatchesOrganized) {
         this.numberOfMatchesOrganized = numberOfMatchesOrganized;
     }
-
+    
     public int getNumberOfSupportStaff() {
         return numberOfSupportStaff;
     }
-
+    
     public void setNumberOfSupportStaff(int numberOfSupportStaff) {
         this.numberOfSupportStaff = numberOfSupportStaff;
     }
@@ -154,12 +156,27 @@ public class User {
     public void setNumberOfSponsors(int numberOfSponsors) {
         this.numberOfSponsors = numberOfSponsors;
     }
-
+    
     public String getRole() {
-        return Role;
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public static int getUserCount() {
+        return userCount;
+    }
+    
+    public static void setUserCount(int userCount) {
+        User.userCount = userCount;
     }
 
-    public void setRole(String role) {
-        Role = role;
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
     }
 }
